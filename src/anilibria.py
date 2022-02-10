43bed7fc4070d37899f5b54094a96ad883d21010
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-02-10 09:29:00 +0600

    Update anilibria.py

--- src/anilibria.py
+++ src/anilibria.py
@@ -8,7 +8,6 @@ class Client():
     def __init__(self):
         self.api = "https://api.anilibria.tv"
         self.api_v2 = "https://www.anilibria.tv/public"
-        self.session = None
         self.headers = {
             "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36",
             "x-requested-with": "XMLHttpRequest"
@@ -32,7 +31,7 @@ class Client():
         request = requests.post(f"{self.api_v2}/login.php", data=data)
         json = request.json()
         self.headers = request.headers
-        self.session = request.headers["set-cookie"]
+        self.session = self.headers["set-cookie"]
         if json.get("code") == 200:
             try:
                 self.session = self.session[0: self.session.index(";")]
@@ -44,9 +43,8 @@ class Client():
 
 	# logout
     def logout(self):
-        request = requests.get(f"{self.api_v2}/logout.php")
+        return requests.get(f"{self.api_v2}/logout.php").json()
         self.session = None
-        return request.json()
 
 	# register account
     def register(self, login: str, email: str, password: str):
@@ -55,20 +53,18 @@ class Client():
             "login": login,
             "mail": email,
             "passwd": password}
-        request = requests.post(
+        return requests.post(
             f"{self.api_v2}/registration.php",
             data=data,
-            headers=self.headers)
-        return request.json()
+            headers=self.headers).json()
 
 	# search anime
     def search_anime(self, search: str, small: int = 1):
         data = {"search": search, "small": small}
-        request = requests.post(
+        return requests.post(
             f"{self.api_v2}/search.php",
             data=data,
-            headers=self.headers)
-        return request.json()
+            headers=self.headers).json()
 
 	# report a error
     def report_a_error(self, message: str, url: str):
@@ -77,13 +73,12 @@ class Client():
             "url": url,
             "g-recaptcha-response": self.generate_captcha(),
             "recaptcha": 2}
-        request = requests.post(
+        return requests.post(
             f"{self.api_v2}/error.php",
             data=data,
-            headers=self.headers)
-        return request.json()
+            headers=self.headers).json()
 
-	# grt favorite anime list
+	# get favorite anime list
     def favorite_anime_list(
             self,
             year: int = None,
@@ -99,11 +94,10 @@ class Client():
                 "sort": sort,
                 "finish": finish
                 }
-        request = requests.post(
+        return requests.post(
             f"{self.api_v2}/catalog.php",
             data=data,
-            headers=self.headers)
-        return request.json()
+            headers=self.headers).json()
 
 	# catalog
     def catalog(
@@ -121,109 +115,91 @@ class Client():
                 "sort": sort,
                 "finish": finish
                 }
-        request = requests.post(
+        return requests.post(
             f"{self.api_v2}/catalog.php",
             data=data,
-            headers=self.headers)
-        return request.json()
+            headers=self.headers).json()
 
 	# get random anime
     def random_anime(self):
         data = {"js": 1}
-        request = requests.post(
+        return requests.post(
             f"{self.api_v2}/random.php",
             data=data,
-            headers=self.headers)
-        return request.json()
+            headers=self.headers).json()
 
 	# get title
     def get_title(self, code: str):
-        request = requests.get(f"{self.api}/v2/getTitle?code={code}")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getTitle?code={code}").json()
 
 	# get updates
     def get_updates(self, filter: str, limit: int = 5):
-        request = requests.get(
-            f"{self.api}/v2/getUpdates?filter={filter},type,status&limit={limit}")
-        return request.json()
+        return requests.get(
+            f"{self.api}/v2/getUpdates?filter={filter},type,status&limit={limit}").json()
 
 	# get changes
     def get_changes(self, filter: str, limit: int = 5):
-        request = requests.get(
-            f"{self.api}/v2/getChanges?filter={filter},type,status&limit={limit}")
-        return request.json()
+        return requests.get(
+            f"{self.api}/v2/getChanges?filter={filter},type,status&limit={limit}").json()
 
 	# get schedule
     def get_schedule(self, days: int):
-        request = requests.get(f"{self.api}/v2/getSchedule?days={days}")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getSchedule?days={days}").json()
 
 	# get caching nodes
     def get_caching_nodes(self):
-        request = requests.get(f"{self.api}/v2/getCachingNodes")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getCachingNodes").json()
 
 	# get random title
     def get_random_title(self):
-        request = requests.get(f"{self.api}/v2/getRandomTitle")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getRandomTitle").json()
 
 	# get YouTube videos
     def get_youtube_videos(self, limit: int = 10):
-        request = requests.get(f"{self.api}/v2/getYouTube?limit={limit}")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getYouTube?limit={limit}").json()
 
 	# get feed
     def get_feed(self, limit: int = 10):
-        request = requests.get(f"{self.api}/v2/getFeed?limit={limit}")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getFeed?limit={limit}").json()
 
 	# get years
     def get_years(self):
-        request = requests.get(f"{self.api}/v2/getYears")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getYears").json()
 
 	# get genres
     def get_genres(self, sorting_type: int = 0):
-        request = requests.get(
-            f"{self.api}/v2/getGenres?sorting_type={sorting_type}")
-        return request.json()
+        return requests.get(
+            f"{self.api}/v2/getGenres?sorting_type={sorting_type}").json()
 
 	# search titles
     def search_titles(self, search: str, limit: int = 10):
-        request = requests.get(
-            f"{self.api}/v2/searchTitles?search={search}&limit={limit}")
-        return request.json()
+        return requests.get(
+            f"{self.api}/v2/searchTitles?search={search}&limit={limit}").json()
 
 	# get team
     def get_team(self):
-        request = requests.get(f"{self.api}/v2/getTeam")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getTeam").json()
 
 	# get seeds stats
     def get_seed_stats(self, users: str = None, limit: int = 10):
         get_seed_stats_link = f"{self.api}/v2/getSeedStats?limit={limit}"
         if users:
             get_seed_stats_link = f"{self.api}/v2/getSeedStats?users={users}"
-        request = requests.get(get_seed_stats_link)
-        return request.json()
+        return requests.get(get_seed_stats_link).json()
 
 	# get rss
     def get_rss(self, rss_type: str, limit: int = 5):
-        request = requests.get(
-            f"{self.api}/v2/getRSS?rss_type={rss_type}&limit={limit}")
-        return request.json()
+        return requests.get(
+            f"{self.api}/v2/getRSS?rss_type={rss_type}&limit={limit}").json()
 
 	# get favorite titles
     def get_favorite_titles(self, session: str):
-        request = requests.get(f"{self.api}/v2/getFavorites?session={session}")
-        return request.json()
+        return requests.get(f"{self.api}/v2/getFavorites?session={session}")
 
 	# add title to favorites
     def add_title_to_favorites(self, session: str, title_Id: int):
-        request = requests.put(
+        return requests.put(
             f"{self.api}/v2/addFavorite?session={session}&title_id={title_Id}")
-        return request.json()
 
 	# delete title from favorites
     def delete_title_from_favorites(self, session: str, title_Id: int):
